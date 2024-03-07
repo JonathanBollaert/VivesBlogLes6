5ca77be19a7d07065f60dd0b9f464ca64b2ab083
JonathanBollaert <159426039+JonathanBollaert@users.noreply.github.com>
2024-03-07 12:08:11 +0100

    Initial commit

---
+++ VivesBlog.Ui.Mvc/Controllers/ArticlesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VivesBlog.Ui.Mvc.Core;
+using VivesBlog.Ui.Mvc.Models;
+
+namespace VivesBlog.Ui.Mvc.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        private readonly VivesBlogDbContext _vivesBlogDbContext;
+
+        public ArticlesController(VivesBlogDbContext vivesBlogDbContext)
+        {
+            _vivesBlogDbContext = vivesBlogDbContext;
+        }
+        public IActionResult Index()
+        {
+            var articles = _vivesBlogDbContext.Articles.ToList();
+            return View(articles);
+        }
+
+    }
+}
